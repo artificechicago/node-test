7c74f0477253dabc4094da1d2d98ef56a611eb91
James Crooks <crooks1379@gmail.com>
2014-01-20 16:42:08 -0600

    anonymous function change

--- requestHandlers.js
+++ requestHandlers.js
@@ -3,9 +3,11 @@ var exec = require("child_process").exec;
 function chime(response) {
     console.log("Request handler 'chime' was called.");
     
-    exec("aplay /usr/share/sounds/linuxmint-login.wav", function (error, stdout, stderr) {
+    exec("aplay /usr/share/sounds/linuxmint-login.wav" function () {
+	//if I understand correctly, we can leave the anonymous function's parameter
+	//list empty since there's a lexical closure in this scope pulling in the
+	// response parameter from one level up
 	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write(stdout);
 	response.end();
 	});
 }
